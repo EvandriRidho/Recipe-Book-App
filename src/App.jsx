588dc8faf4aa120e0036740aa45120fbcd0d228e
unknown <evandriridho555@gmail.com>
2024-08-13 22:46:43 +0700

    Set Up Project Vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+let App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
