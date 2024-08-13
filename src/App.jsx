2781cc2a9025786809a559f69d063970a30acfbd
unknown <evandriridho555@gmail.com>
2024-08-14 00:59:24 +0700

    Update

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-let App = () => {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-3xl font-bold">Hello World</h1>
-    </div>
-  );
-};
-
-export default App;
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Navbar from "../components/Layouts/navbar";
+
+let HomePage = () => {
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  );
+};
+
+export default HomePage;
